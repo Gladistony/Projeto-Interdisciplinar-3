d4058fc9913b97c137194dd4991015eb32ecedf8
José Miguel da Silva <jmigueldasilva953@gmail.com>
2024-11-24 21:31:00 -0300

    correção de bugs
    problema na simulação resolvido.

--- App/app/src/main/java/com/teste/projeto_3/TelaPrincipal.java
+++ App/app/src/main/java/com/teste/projeto_3/TelaPrincipal.java
@@ -20,6 +20,5 @@ public class TelaPrincipal extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        getSupportActionBar().hide();
     }
 }
