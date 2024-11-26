c09a3bfc57c445006b3d70a8868776eb4e76d258
Gustavo França <gustavofr0411@gmail.com>
2024-11-25 23:07:49 -0300

    Adição de ocultamento/exibição na digitação da senha

--- App/app/src/main/java/com/teste/projeto_3/TelaLogin.java
+++ App/app/src/main/java/com/teste/projeto_3/TelaLogin.java
@@ -1,11 +1,14 @@
 package com.teste.projeto_3;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.EditText;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -33,4 +36,30 @@ public class TelaLogin extends AppCompatActivity {
         intent.putExtra("senha", senha.getText().toString());
         startActivity(intent);
     }
+
+    public void togglePassword(View v){
+        EditText caixaTexto = findViewById(R.id.senha);
+        int posicaoCursor = caixaTexto.getSelectionStart();
+
+        if (caixaTexto.getInputType() == InputType.TYPE_CLASS_TEXT + InputType.TYPE_TEXT_VARIATION_PASSWORD) {
+            // se inputType == 1 + 128 (código de textPassword) então inputType = 1 + 144 (código de textVisiblePassword)
+            caixaTexto.setInputType(InputType.TYPE_CLASS_TEXT + InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+
+            // troca o drawable pra olho fechado
+            Drawable eyeHiddenDrawable = ContextCompat.getDrawable(this, R.drawable.ic_eye_hidden);
+            caixaTexto.setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, eyeHiddenDrawable, null);
+        } else {
+            // inputType é (código de textVisiblePassword) então inputType = 1 + 128 (código de textPassword)
+            caixaTexto.setInputType(InputType.TYPE_CLASS_TEXT + InputType.TYPE_TEXT_VARIATION_PASSWORD);
+
+            // troca o drawable pra olho aberto
+            Drawable eyeOpenDrawable = ContextCompat.getDrawable(this, R.drawable.ic_eye);
+            caixaTexto.setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, eyeOpenDrawable, null);
+        }
+
+        // volta o cursor para a mesma posição antes de ocultar/exibir
+        caixaTexto.setSelection(posicaoCursor);
+
+    }
+
 }
