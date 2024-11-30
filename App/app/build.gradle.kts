1848b58852e3fe48b326891dd8c06c73122a5c37
José Miguel da Silva <jmigueldasilva953@gmail.com>
2024-11-30 15:28:48 -0300

    atentando conectar o GSON e o OkHttp3 para fazer uma requisição HTTP

--- App/app/build.gradle.kts
+++ App/app/build.gradle.kts
@@ -38,6 +38,17 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     testImplementation(libs.junit)
+
+    // DEPENDÊNCIAS PARA ACESSAR A API
+    // DEPENDÊNCIAS PARA ACESSAR A API
+    implementation("com.google.code.gson:gson:2.10.1") // Atualizado
+    implementation("com.squareup.okhttp3:okhttp:4.11.0") // Atualizado
+
+    // ROOM PARA BANCO DE DADOS LOCAL
+    implementation("androidx.room:room-runtime:2.5.2") // Última versão
+    kapt("androidx.room:room-compiler:2.5.2")          // Para geração de código
+    implementation("androidx.room:room-ktx:2.5.2")     // Extensões Kotlin para Room
+
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
