c272133e8c76c80d5d432426065d4e1096a4ad08
Gustavo França <gustavofr0411@gmail.com>
2025-01-11 21:52:25 -0300

    Splash Screen (tela de introdução do app) inicial
    Implementada a animação básica para a Splash Screen

--- App/app/build.gradle.kts
+++ App/app/build.gradle.kts
@@ -50,6 +50,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.activity:activity:1.9.3")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
     testImplementation(libs.junit)
 
     // DEPENDÊNCIAS PARA ACESSAR A API
--- App/app/src/main/java/com/teste/projeto_3/MainActivity.java
+++ App/app/src/main/java/com/teste/projeto_3/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Toast;
 
@@ -32,13 +33,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.motionLayout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
         dh = new DataHandler(getApplicationContext());
-        checkLoggedIn();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, LoginCadastro.class));
+                finish();
+            }
+        }, 3000);
     }
 
     public void checkLoggedIn() {
