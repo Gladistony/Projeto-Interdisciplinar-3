4d83042371f357bc8de0f7e00489a3b512d03e84
Gustavo França <gustavofr0411@gmail.com>
2025-02-26 15:52:39 -0300

    Alterado design da tela de perfil

--- App/app/src/main/java/com/teste/projeto_3/FragPerfil.java
+++ App/app/src/main/java/com/teste/projeto_3/FragPerfil.java
@@ -2,13 +2,10 @@ package com.teste.projeto_3;
 
 import android.content.Intent;
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-
-import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,61 +13,24 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.gson.Gson;
 import com.squareup.picasso.Picasso;
 import com.teste.projeto_3.http.EnviarRequisicao;
 import com.teste.projeto_3.model.Data;
 import com.teste.projeto_3.model.User;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragPerfil#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FragPerfil extends Fragment {
 
     EnviarRequisicao er;
     private Gson gson = new Gson();
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public FragPerfil() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragPerfil.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragPerfil newInstance(String param1, String param2) {
-        FragPerfil fragment = new FragPerfil();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
         er = new EnviarRequisicao(requireContext());
     }
 
@@ -78,8 +38,8 @@ public class FragPerfil extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        Button meuBotao = view.findViewById(R.id.deslogar);
-        meuBotao.setOnClickListener(v -> deslogar());
+        Button logoff = view.findViewById(R.id.buttonLogOff);
+        logoff.setOnClickListener(v -> confirmLogOff());
 
     }
 
@@ -98,7 +58,7 @@ public class FragPerfil extends Fragment {
 
             if (dados.getData() == null) {
             nome.setText(dados.getNome_completo());
-            email.setText(dados.getEmail());;
+            email.setText(dados.getEmail());
             if (dados.getUrl_foto() != null) {
                 try {
                     Picasso.get().load(dados.getUrl_foto()).into(fotoPerfil);
@@ -109,7 +69,7 @@ public class FragPerfil extends Fragment {
             } else {
                 Data dadosData = dados.getData();
                 nome.setText(dadosData.getNome_completo());
-                email.setText(dadosData.getEmail());;
+                email.setText(dadosData.getEmail());
                 if (dadosData.getUrl_foto() != null) {
                     try {
                         Picasso.get().load(dadosData.getUrl_foto()).into(fotoPerfil);
@@ -123,7 +83,7 @@ public class FragPerfil extends Fragment {
         return view;
     }
 
-    public void deslogar() {
+    private void deslogar() {
         if (er.possuiInternet(requireContext())) {
             User userLogin = new User();
             userLogin.setId(er.obterMemoriaInterna("idConexao"));
@@ -161,6 +121,17 @@ public class FragPerfil extends Fragment {
                     Toast.makeText(requireContext(), "Verifique sua conexão com a internet.", Toast.LENGTH_SHORT).show()
             );
         }
+    }
+
+    private void confirmLogOff(){
+        new android.app.AlertDialog.Builder(getContext())
+                .setTitle("Sair da conta")
+                .setMessage("Você tem certeza que deseja sair desta conta?")
+                .setPositiveButton("Sair", (dialog, which) -> {
+                    deslogar();
+                })
+                .setNegativeButton("Cancelar", (dialog, which) -> dialog.dismiss())
+                .show();
     }
 
 }
