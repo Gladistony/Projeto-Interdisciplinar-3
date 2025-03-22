b3fdc84a41c6badcf2f766eef12f08e111f67cb1
Gustavo França <gustavofr0411@gmail.com>
2025-03-22 11:52:23 -0300

    Exibição de animação de carregamento nos botões do Android

--- App/app/src/main/java/com/teste/projeto_3/TelaValidacao.java
+++ App/app/src/main/java/com/teste/projeto_3/TelaValidacao.java
@@ -3,7 +3,9 @@ package com.teste.projeto_3;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +19,11 @@ import com.teste.projeto_3.model.User;
 public class TelaValidacao extends AppCompatActivity {
     EnviarRequisicao er;
 
+    ProgressBar progressBarValidarCodigo;
+    ProgressBar progressBarValidarEmail;
+    Button botaoValidarCodigo;
+    Button botaoValidarEmail;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,11 +35,15 @@ public class TelaValidacao extends AppCompatActivity {
             return insets;
         });
         er = new EnviarRequisicao(getApplicationContext());
-
+        progressBarValidarCodigo = findViewById(R.id.progressBarValidarCodigo);
+        progressBarValidarEmail = findViewById(R.id.progressBarValidarEmail);
+        botaoValidarCodigo = findViewById(R.id.botaoValidarCodigo);
+        botaoValidarEmail = findViewById(R.id.botaoValidarEmail);
     }
 
     public void validarPorEmail(View v) {
         if (er.possuiInternet(getApplicationContext())) {
+            runOnUiThread(() -> changeButtonMode(botaoValidarEmail, progressBarValidarEmail, getString(R.string.text_validar_email)));
             Intent intentInfoTelaLogin = getIntent();
             // Criando o objeto User
             User userLogin = new User();
@@ -48,7 +59,10 @@ public class TelaValidacao extends AppCompatActivity {
                 // Fazer a requisição
                 er.post("login", userJson, response -> {
                     if (response.startsWith("Erro")) {
-                        runOnUiThread(() -> Toast.makeText(this, response, Toast.LENGTH_LONG).show());
+                        runOnUiThread(() -> {
+                            Toast.makeText(this, response, Toast.LENGTH_LONG).show();
+                            changeButtonMode(botaoValidarEmail, progressBarValidarEmail, getString(R.string.text_validar_email));
+                        });
                     } else {
                         // Processar resposta da requisição
                         User responseLogin = gson.fromJson(response, User.class);
@@ -61,34 +75,52 @@ public class TelaValidacao extends AppCompatActivity {
                                     break;
 
                                 case 3: // Conta não está ativa
-                                    runOnUiThread(() -> Toast.makeText(this, "Conta ainda não ativada. Aguarde e tente novamente.", Toast.LENGTH_LONG).show());
+                                    runOnUiThread(() -> {
+                                        Toast.makeText(this, "Conta ainda não ativada. Aguarde e tente novamente.", Toast.LENGTH_LONG).show();
+                                        changeButtonMode(botaoValidarEmail, progressBarValidarEmail, getString(R.string.text_validar_email));
+                                    });
                                     break;
 
                                 case 4: // Conta não encontrada
-                                    runOnUiThread(() -> Toast.makeText(this, responseLogin.getMessage(), Toast.LENGTH_SHORT).show());
+                                    runOnUiThread(() -> {
+                                        Toast.makeText(this, responseLogin.getMessage(), Toast.LENGTH_SHORT).show();
+                                        changeButtonMode(botaoValidarEmail, progressBarValidarEmail, getString(R.string.text_validar_email));
+                                    });
                                     break;
 
                                 case 12: // Conexão não encontrada
-                                    runOnUiThread(() -> Toast.makeText(this, "Houve um problema na conexão. Por favor, reinicie o aplicativo.", Toast.LENGTH_SHORT).show());
+                                    runOnUiThread(() -> {
+                                        Toast.makeText(this, "Houve um problema na conexão. Por favor, reinicie o aplicativo.", Toast.LENGTH_SHORT).show();
+                                        changeButtonMode(botaoValidarEmail, progressBarValidarEmail, getString(R.string.text_validar_email));
+                                    });
                                     break;
                             }
                         }
                 });
             } catch (Exception e) {
-                runOnUiThread(() -> Toast.makeText(this, "Ocorreu um erro ao conectar-se ao servidor.", Toast.LENGTH_SHORT).show());
+                runOnUiThread(() -> {
+                    Toast.makeText(this, "Ocorreu um erro ao conectar-se ao servidor.", Toast.LENGTH_SHORT).show();
+                    changeButtonMode(botaoValidarEmail, progressBarValidarEmail, getString(R.string.text_validar_email));
+                });
             }
         } else {
-            runOnUiThread(() -> Toast.makeText(this, "Verifique sua conexão com a internet.", Toast.LENGTH_SHORT).show());
+            runOnUiThread(() -> {
+                Toast.makeText(this, "Verifique sua conexão com a internet.", Toast.LENGTH_SHORT).show();
+            });
         }
     }
 
     public void validarPorCodigo(View v) {
         if (er.possuiInternet(getApplicationContext())) {
+            runOnUiThread(() -> changeButtonMode(botaoValidarCodigo, progressBarValidarCodigo, getString(R.string.validar_conta)));
             EditText validacao = findViewById(R.id.codigo_validacao);
             Intent intentInfoTelaLogin = getIntent();
 
             if (validacao.getText().toString().isEmpty()) {
-                Toast.makeText(this, "Por favor, preencha todos os campos obrigatórios", Toast.LENGTH_SHORT).show();
+                runOnUiThread(() -> {
+                    Toast.makeText(this, "Por favor, preencha todos os campos obrigatórios", Toast.LENGTH_SHORT).show();
+                    changeButtonMode(botaoValidarCodigo, progressBarValidarCodigo, getString(R.string.validar_conta));
+                });
             } else {
 
                 String usuario = intentInfoTelaLogin.getStringExtra("usuario");
@@ -99,25 +131,39 @@ public class TelaValidacao extends AppCompatActivity {
                     // Fazer a requisição
                     er.get("ativar", usuario + "/" + codigoValidacao, response -> {
                         if (response.startsWith("Erro")) {
-                            runOnUiThread(() -> Toast.makeText(this, response, Toast.LENGTH_LONG).show());
+                            runOnUiThread(() -> {
+                                Toast.makeText(this, response, Toast.LENGTH_LONG).show();
+                                changeButtonMode(botaoValidarCodigo, progressBarValidarCodigo, getString(R.string.validar_conta));
+                            });
                         } else {
                             // Processar resposta da requisição
                             String resultado = response.substring(905); // 905 é o índice da String HTML em que diz o resultado da requisição
                             if (resultado.startsWith("Código de ativação incorreto")) {
-                                runOnUiThread(() -> Toast.makeText(this, "Código de ativação incorreto", Toast.LENGTH_LONG).show());
+                                runOnUiThread(() -> {
+                                    Toast.makeText(this, "Código de ativação incorreto", Toast.LENGTH_LONG).show();
+                                    changeButtonMode(botaoValidarCodigo, progressBarValidarCodigo, getString(R.string.validar_conta));
+                                });
                             } else if (resultado.startsWith("Operação realizada com sucesso") || resultado.startsWith("Conta já está ativa")) {
                                 login(usuario, senha);
                             } else {
-                                runOnUiThread(() -> Toast.makeText(this, "Erro na validação por código.", Toast.LENGTH_LONG).show());
+                                runOnUiThread(() -> {
+                                    Toast.makeText(this, "Erro na validação por código.", Toast.LENGTH_LONG).show();
+                                    changeButtonMode(botaoValidarCodigo, progressBarValidarCodigo, getString(R.string.validar_conta));
+                                });
                             }
                         }
                     });
                 } catch (Exception e) {
-                    runOnUiThread(() -> Toast.makeText(this, "Ocorreu um erro ao conectar-se ao servidor.", Toast.LENGTH_LONG).show());
+                    runOnUiThread(() -> {
+                        Toast.makeText(this, "Ocorreu um erro ao conectar-se ao servidor.", Toast.LENGTH_LONG).show();
+                        changeButtonMode(botaoValidarCodigo, progressBarValidarCodigo, getString(R.string.validar_conta));
+                    });
                 }
             }
         } else {
-            runOnUiThread(() -> Toast.makeText(this, "Verifique sua conexão com a internet.", Toast.LENGTH_SHORT).show());
+            runOnUiThread(() -> {
+                Toast.makeText(this, "Verifique sua conexão com a internet.", Toast.LENGTH_SHORT).show();
+            });
         }
     }
 
@@ -136,7 +182,10 @@ public class TelaValidacao extends AppCompatActivity {
             // Fazer a requisição
             er.post("login", userJson, response -> {
                 if (response.startsWith("Erro")) {
-                    runOnUiThread(() -> Toast.makeText(this, response, Toast.LENGTH_LONG).show());
+                    runOnUiThread(() -> {
+                        Toast.makeText(this, response, Toast.LENGTH_LONG).show();
+                        changeButtonMode(botaoValidarCodigo, progressBarValidarCodigo, getString(R.string.validar_conta));
+                    });
                 } else {
                     // Processar resposta da requisição
                     User responseLogin = gson.fromJson(response, User.class);
@@ -150,34 +199,59 @@ public class TelaValidacao extends AppCompatActivity {
                                 break;
 
                             case 1: // Senha incorreta
-                                runOnUiThread(() -> Toast.makeText(this, "Senha incorreta. Entre novamente e verifique-a.", Toast.LENGTH_SHORT).show());
+                                runOnUiThread(() -> {
+                                    Toast.makeText(this, "Senha incorreta. Entre novamente e verifique-a.", Toast.LENGTH_LONG).show();
+                                    changeButtonMode(botaoValidarCodigo, progressBarValidarCodigo, getString(R.string.validar_conta));
+                                    botaoValidarEmail.setAlpha(0.5f);
+                                    botaoValidarCodigo.setAlpha(0.5f);
+                                    botaoValidarCodigo.setClickable(false);
+                                    botaoValidarEmail.setClickable(false);
+                                });
                                 break;
 
                             case 2: // Conta bloqueada por excesso de tentativas
                                 if (responseLogin.getMessage().endsWith("ativação novamente necessária")) {
-                                    runOnUiThread(() -> Toast.makeText(this, "Ativação de conta necessária por excesso de tentativas.", Toast.LENGTH_SHORT).show());
+                                    runOnUiThread(() -> {
+                                        Toast.makeText(this, "Ativação de conta necessária por excesso de tentativas.", Toast.LENGTH_SHORT).show();
+                                        changeButtonMode(botaoValidarCodigo, progressBarValidarCodigo, getString(R.string.validar_conta));
+                                    });
                                 } else {
-                                    runOnUiThread(() -> Toast.makeText(this, "Conta bloqueada por excesso de tentativas. Aguarde " + segToMin(responseLogin.getRestante()) + ".", Toast.LENGTH_SHORT).show());
+                                    runOnUiThread(() -> {
+                                        Toast.makeText(this, "Conta bloqueada por excesso de tentativas. Aguarde " + segToMin(responseLogin.getRestante()) + ".", Toast.LENGTH_SHORT).show();
+                                        changeButtonMode(botaoValidarCodigo, progressBarValidarCodigo, getString(R.string.validar_conta));
+                                    });
                                 }
                                 break;
 
                             case 3: // Conta não está ativa
-                                runOnUiThread(() -> Toast.makeText(this, "Conta ainda não ativada. Aguarde e tente novamente.", Toast.LENGTH_LONG).show());
+                                runOnUiThread(() -> {
+                                    Toast.makeText(this, "Conta ainda não ativada. Aguarde e tente novamente.", Toast.LENGTH_LONG).show();
+                                    changeButtonMode(botaoValidarCodigo, progressBarValidarCodigo, getString(R.string.validar_conta));
+                                });
                                 break;
 
                             case 4: // Conta não encontrada
-                                runOnUiThread(() -> Toast.makeText(this, responseLogin.getMessage(), Toast.LENGTH_SHORT).show());
+                                runOnUiThread(() -> {
+                                    Toast.makeText(this, responseLogin.getMessage(), Toast.LENGTH_SHORT).show();
+                                    changeButtonMode(botaoValidarCodigo, progressBarValidarCodigo, getString(R.string.validar_conta));
+                                });
                                 break;
 
                             case 12: // Conexão não encontrada
-                                runOnUiThread(() -> Toast.makeText(this, "Houve um problema na conexão. Por favor, reinicie o aplicativo.", Toast.LENGTH_SHORT).show());
+                                runOnUiThread(() -> {
+                                    Toast.makeText(this, "Houve um problema na conexão. Por favor, reinicie o aplicativo.", Toast.LENGTH_SHORT).show();
+                                    changeButtonMode(botaoValidarCodigo, progressBarValidarCodigo, getString(R.string.validar_conta));
+                                });
                                 break;
                         }
                     }
                 }
             });
         } catch (Exception e) {
-            runOnUiThread(() -> Toast.makeText(this, "Ocorreu um erro ao conectar-se ao servidor.", Toast.LENGTH_SHORT).show());
+            runOnUiThread(() -> {
+                Toast.makeText(this, "Ocorreu um erro ao conectar-se ao servidor.", Toast.LENGTH_SHORT).show();
+                changeButtonMode(botaoValidarCodigo, progressBarValidarCodigo, getString(R.string.validar_conta));
+            });
         }
     }
 
@@ -185,6 +259,20 @@ public class TelaValidacao extends AppCompatActivity {
         int minutos = (int) (seg / 60);
         long segundos = Math.round(seg % 60);
         return minutos + "m " + segundos + "s";
+    }
+
+    private void changeButtonMode(Button button, ProgressBar progressBar, String buttonString) {
+        if (progressBar.getVisibility() == View.GONE){
+            progressBar.setVisibility(View.VISIBLE);
+            button.setText("");
+            button.setClickable(false);
+            button.setAlpha(0.8f);
+        } else {
+            progressBar.setVisibility(View.GONE);
+            button.setText(buttonString);
+            button.setClickable(true);
+            button.setAlpha(1f);
+        }
     }
 }
 
